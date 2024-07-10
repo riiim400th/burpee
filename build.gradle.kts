8140fde43cd8a72be161dd769b7c21d2bf62f563
RiiiM <riiimparm+tech@gmail.com>
2024-07-11 08:39:26 +0900

    feat: add Outline, Path in scope

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.example"
-version = "1.1.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,12 @@ package burpee
 
 import burp.api.montoya.BurpExtension
 import burp.api.montoya.MontoyaApi
-import javax.swing.JLabel
 
 class Main : BurpExtension{
     override fun initialize(api: MontoyaApi) {
         val logging = api.logging()
         api.extension().setName("burpee")
-        logging.logToOutput("loaded burpee v1.1.0\r\n")
+        logging.logToOutput("loaded burpee v1.1.1\r\n")
         val menuTask = MenuTask(api)
         api.userInterface().registerContextMenuItemsProvider(menuTask)
         api.userInterface().registerSuiteTab("burpee", TabTask(api))
